78496ee8d1d13c2ca3e5983bcb960b98a7386e10
seydlitz19 <seydlitz19@up.edu>
2017-09-07 16:24:19 -0700

    Initial commit

---
+++ app/src/main/java/cs371/carstereo/CarStereoGUI.java
@@ -0,0 +1,13 @@
+package cs371.carstereo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarStereoGUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_stereo_gui);
+    }
+}
